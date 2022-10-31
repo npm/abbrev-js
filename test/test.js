602c0feb27f370588b3659781f8f1d52b80df3a7
Luke Karrys <luke@lukekarrys.com>
2022-10-31 11:23:16 -0700

    feat!: add @npmcli/template-oss (#36)
    BREAKING CHANGE: Refactored with the following breaking changes:
    - `abbrev` is now compatible with the following semver range for node:
    `^14.17.0 || ^16.13.0 || >=18.0.0`
    - `monkeyPatch` API has been removed

--- test/test.js
+++ test/test.js
@@ -1,55 +1,54 @@
-var abbrev = require('../')
-var t = require('tap')
-var util = require("util")
-
-var count = 0
+const abbrev = require('../')
+const t = require('tap')
+const util = require('util')
 
 function test (list, expect) {
-  var actual = abbrev(list)
-  t.deepEqual(actual, expect,
-    "abbrev("+util.inspect(list)+") === " + util.inspect(expect) + "\n"+
-    "actual: "+util.inspect(actual))
-  actual = abbrev.apply(exports, list)
-  t.deepEqual(abbrev.apply(exports, list), expect,
-    "abbrev("+list.map(JSON.stringify).join(",")+") === " + util.inspect(expect) + "\n"+
-    "actual: "+util.inspect(actual))
+  let actual = abbrev(list)
+  t.same(actual, expect,
+    'abbrev(' + util.inspect(list) + ') === ' + util.inspect(expect) + '\n' +
+    'actual: ' + util.inspect(actual))
+
+  actual = abbrev(...list)
+  t.same(abbrev(...list), expect,
+    'abbrev(' + list.map(JSON.stringify).join(',') + ') === ' + util.inspect(expect) + '\n' +
+    'actual: ' + util.inspect(actual))
 }
 
-test([ "ruby", "ruby", "rules", "rules", "rules" ],
-{ rub: 'ruby'
-, ruby: 'ruby'
-, rul: 'rules'
-, rule: 'rules'
-, rules: 'rules'
-})
-test(["fool", "foom", "pool", { toString: function () { return "pope" } }],
-{ fool: 'fool'
-, foom: 'foom'
-, poo: 'pool'
-, pool: 'pool'
-, pop: 'pope'
-, pope: 'pope'
-})
-test(["a", "ab", "abc", "abcd", "abcde", "acde"],
-{ a: 'a'
-, ab: 'ab'
-, abc: 'abc'
-, abcd: 'abcd'
-, abcde: 'abcde'
-, ac: 'acde'
-, acd: 'acde'
-, acde: 'acde'
-})
+test(['ruby', 'ruby', 'rules', 'rules', 'rules'],
+  { rub: 'ruby',
+    ruby: 'ruby',
+    rul: 'rules',
+    rule: 'rules',
+    rules: 'rules',
+  })
+test(['fool', 'foom', 'pool', { toString: () => 'pope' }],
+  { fool: 'fool',
+    foom: 'foom',
+    poo: 'pool',
+    pool: 'pool',
+    pop: 'pope',
+    pope: 'pope',
+  })
+test(['a', 'ab', 'abc', 'abcd', 'abcde', 'acde'],
+  { a: 'a',
+    ab: 'ab',
+    abc: 'abc',
+    abcd: 'abcd',
+    abcde: 'abcde',
+    ac: 'acde',
+    acd: 'acde',
+    acde: 'acde',
+  })
+test(['a', 'ab', 'abc', 'abcd', 'abcde', 'acde'].reverse(),
+  { a: 'a',
+    ab: 'ab',
+    abc: 'abc',
+    abcd: 'abcd',
+    abcde: 'abcde',
+    ac: 'acde',
+    acd: 'acde',
+    acde: 'acde',
+  })
 
 t.notOk([].abbrev)
 t.notOk({}.abbrev)
-
-abbrev.monkeyPatch()
-
-t.isa([].abbrev, 'function')
-t.isa({}.abbrev, 'function')
-
-var list = ['pool', 'pope', 'fool', 'floop', 'flu']
-t.same(list.abbrev(), abbrev(list))
-list = list.reduce(function (s, k) { s[k]=true; return s }, {})
-t.same(list.abbrev(), abbrev(Object.keys(list)))
